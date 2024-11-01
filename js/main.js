acfb907b0d31ed32899e88ead88ec24abf0a0b4c
Michael Moser <109764421+MichaelMoser25@users.noreply.github.com>
2024-11-01 18:09:53 -0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Create grapes js object with some configuration
+const editor = grapesjs.init({
+    container: "#editor",
+    fromElement: true,
+    width: "auto",
+    storageManager: false,
+    plugins:["gjs-preset-webpage"],
+    plugins0pts:{
+        "gjs-preset-webpage":{
+            
+        }
+    }
+})
